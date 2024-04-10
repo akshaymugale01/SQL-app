14eb593b112cbb35f36a6c7b08a1fb771c8159b1
akshaymugale01 <139070784+akshaymugale01@users.noreply.github.com>
2024-04-10 22:17:28 +0530

    Update DatabaseConnection.ts

--- backend/src/models/DatabaseConnection.ts
+++ backend/src/models/DatabaseConnection.ts
@@ -1,19 +1,15 @@
-// DatabaseConnection.ts
-
 class DatabaseConnection {
     private type: string;
     private username: string;
     private password: string;
-    // Add other necessary properties such as host, database name, etc.
+
   
     constructor(type: string, username: string, password: string) {
       this.type = type;
       this.username = username;
       this.password = password;
-      // Initialize other properties as needed
     }
-  
-    // Getter methods
+ 
     getType(): string {
       return this.type;
     }
@@ -25,8 +21,7 @@ class DatabaseConnection {
     getPassword(): string {
       return this.password;
     }
-  
-    // Setter methods
+
     setType(type: string): void {
       this.type = type;
     }
@@ -38,9 +33,6 @@ class DatabaseConnection {
     setPassword(password: string): void {
       this.password = password;
     }
-  
-    // Add other getter and setter methods as needed
-  }
   
   export default DatabaseConnection;
   
